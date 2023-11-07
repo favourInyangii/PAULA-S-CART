a7dbc5148d53bdb8ad1b4d2558fb1861bf3a4851
favour ololade <ololadefavourajayi@gmail.com>
2023-11-07 12:03:19 +0100

    addding a json file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 let iconCart = document .querySelector('.icon-cart');
-let body =document .querySelector('body');
+let closeCart= document .querySelector('.close');
+let body = document .querySelector('body');
+let listproductHTML = document .querySelector('.listproduct');
 
+let listproducts = [];
 iconCart.addEventListener('click',() =>{
     body.classList.toggle('showCart')
 })
+closeCart .addEventListener('click',()=>{
+    body.classList.toggle('showCart')
+})
+
+
+
+
+const initApp =() =>{
+//get data from json
+fetch('products.json')
+.then(response => response.json())
+.then(data =>{
+    listproducts = data;
+})
+console.log(listproducts);
+}
+initApp();
